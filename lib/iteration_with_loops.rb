4325d5557c51e4e1ddf0f29a1a12e6dcd19cbbe4
Matthew Sallin <msallin@gmail.com>
2020-09-23 00:28:00 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,10 +7,10 @@ def find_min_in_nested_arrays(src)
   while counterA < src.length do
     puts "counterA = #{counterA}"
     #puts "src[counterA] = #{src[counterA]}"
-    newArr[counterA] = 0
     counterB = 0
+    newArr[counterA][counterB] = 0
     while counterB < src[counterA].length do
-      puts "src[counterA].length = #{src[counterA].length}"
+      #puts "src[counterA].length = #{src[counterA].length}"
       if src[counterA][counterB] > newArr[counterA]
         puts "src[counterA][counterB] = #{src[counterA][counterB]}"
         newArr[counterA] = src[counterA][counterB]
